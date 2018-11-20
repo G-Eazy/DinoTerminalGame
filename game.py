87c4d8b29accac11580da236cb0605ed9f96eacd
Greger Martinussen <greger.martinussen3@gmail.com>
2018-11-20 21:53:30 +0100

    Added game.py, updated gitignore

--- game.py
+++ game.py
@@ -1,39 +1,44 @@
 from game_engine import Game
 
-g = Game()
-
-print(g)
-print(g.dimensions())
+g = Game(log=True)
 
 dino = g.create_box(15, 25, 175, 15)
 
+boxes = []
+boxes.append(g.create_box(5, 5, 5, 5))
+#boxes.append(g.create_box(5, 5, 25, 25))
 ## need to save and display highscore
 time_elapsed = 0
 score = 0
-delay = 200 
+delay = 100
+
 while True:
     k = g.next_frame(delay) # enter delay in ms
-    
+    break
+
+g.quit()
+"""
     # Update stats
     time_elapsed += delay 
     score += time_elapsed / 100
     
     # Update boxes
-    for box in g.boxes:
+    for box in boxes:
         box.set_x(box.x-5)
 
     if time_elapsed % 1000 == 0:
-        g.boxes.append(g.create_box(25, 25))
+        boxes.append(g.create_box(25, 25))
 
     ### User Action ###
     if k is None:
-        print(time_elapsed)
+        #print(time_elapsed)
+        a=0
     elif k == Game.SPACE_KEY:
-        print("space key pressed")
+        #print("space key pressed")
         g.jump()
       
     # Check for collisions and end of frames
-    for _box in g.boxes:
+    for _box in boxes:
         if _box.x <= 0:
             _box.destroy()
             g.remove_box()
@@ -43,3 +48,5 @@ while True:
                 g.quit(score)
 
     # jump
+
+"""
